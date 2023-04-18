b1112a111391b05c86d1915b713241b617f9efa0
ssg-js <likemath1997@gmail.com>
2023-04-18 17:51:11 +0900

    #3.8

---
+++ toonflix/lib/widgets/currency_card.dart
@@ -0,0 +1,81 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CurrencyCard extends StatelessWidget {
+
+  final String name, code, amount;
+  final IconData icon;
+  final Bool isInverted;
+
+  const CurrencyCard({
+    super.key,
+    required this.name,
+    required this.code,
+    required this.amount,
+    required this.icon,
+    required this.isInverted,
+  });
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        clipBehavior: Clip.hardEdge,
+        decoration: BoxDecoration(
+          color: isInverted ? Colors.white : const Color(0xFF1F2123),
+          borderRadius: BorderRadius.circular(25),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(name,
+                    style: const TextStyle(
+                      color: isInverted ? Colors.black : Colors.white,
+                      fontSize: 32,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  const SizedBox(height: 10,),
+                  Row(
+                    children: [
+                      Text(amount,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                      const SizedBox(width:5,),
+                      Text(code,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Transform.scale(
+                scale: 2,
+                child: Transform.translate(
+                  offset: const Offset(0, 11),
+                  child: Icon(
+                    icon,
+                    color: Colors.white,
+                    size: 88,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        )
+    );
+  }
+}
