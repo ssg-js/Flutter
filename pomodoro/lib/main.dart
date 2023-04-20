20318bcf8a4aa165c4c1f0b31fcd0d8ae1c0a458
ssg-js <likemath1997@gmail.com>
2023-04-20 21:19:51 +0900

    pomodoro start

---
+++ pomodoro/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:pomodoro/screens/home_screen.dart';
+
+// omar sharif 씨 재능있다고 함(behance)
+void main() {
+  runApp(App());
+}
+
+class App extends StatefulWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        backgroundColor: const Color(0xFFE7626C),
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
+            color: Color(0xFF232B55),
+          ),
+        ),
+        cardColor: const Color(0xFFF4EDDB),
+      ),
+      home: HomeScreen(),
+    );;
+  }
+}
+
+
